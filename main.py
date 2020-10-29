a4f4b4e3c3c273497bc2c5e102798ad94fa37ece
Verena Böke <verena.boeke@chefkoch.de>
2020-10-29 10:52:49 +0100

    initialise bootstrap, navbar styling

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ def hello_world():
         username = request.form.get("username")
         app.logger.info(f"Form send with username: {username}")
         print(username)
-        return render_template("hello_world.html")
+        return render_template("hello_world.html", navigation="home")
     elif request.method == "GET":
-        return render_template("hello_world.html")
+        return render_template("hello_world.html", navigation="home")
 
 @app.route('/about', methods=["GET"])
 def about():
-    return render_template("about.html")
+    return render_template("about.html", navigation="about")
 
 @app.route('/faq', methods=["GET"])
 def faq():
-    return render_template("faq.html")
+    return render_template("faq.html", navigation="faq")
 
 
 #shortcut "mai"+tab
