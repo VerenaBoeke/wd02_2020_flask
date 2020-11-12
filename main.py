62b748a2a8eab66b981fce44f09231127e17936d
Verena Böke <verena.boeke@chefkoch.de>
2020-11-12 17:03:20 +0100

    homework: added register

--- main.py
+++ main.py
@@ -3,25 +3,16 @@ import hashlib
 import uuid
 
 from flask import Flask, render_template, request, make_response, redirect, url_for
-from model import db, User, Post, Comment
 
-WEBSITE_LOGIN_COOKIE_NAME = "science/session_token"
-COOKIE_DURATION = 900  # in seconds
+from model import db, User, Post, Comment
 
 app = Flask(__name__)
 
 db.create_all()
 
-def getUserByCookie():
-    session_token = request.cookies.get(WEBSITE_LOGIN_COOKIE_NAME)
-    user = None
+WEBSITE_LOGIN_COOKIE_NAME = "science/session_token"
+COOKIE_DURATION = 900  # in seconds
 
-    if session_token is not None:
-        user = db.query(User) \
-            .filter_by(session_cookie=session_token) \
-            .filter(User.session_expiry_datetime >= datetime.datetime.now()) \
-            .first()
-    return user
 
 def require_session_token(func):
     """Decorator to require authentication to access routes"""
@@ -34,7 +25,10 @@ def require_session_token(func):
             app.logger.error('no token in request')
             return redirect(url_for('login', redirectTo=redirect_url))
 
-        user = getUserByCookie()
+        user = db.query(User) \
+            .filter_by(session_cookie=session_token) \
+            .filter(User.session_expiry_datetime >= datetime.datetime.now()) \
+            .first()
 
         if not user:
             app.logger.error(f'token {session_token} not valid')
@@ -49,17 +43,91 @@ def require_session_token(func):
     wrapper.__name__ = func.__name__
     return wrapper
 
+
+def provide_user(func):
+    """Decorator to read user info if available"""
+
+    def wrapper(*args, **kwargs):
+        session_token = request.cookies.get(WEBSITE_LOGIN_COOKIE_NAME)
+
+        if not session_token:
+            request.user = None
+            return func(*args, **kwargs)
+
+        user = db.query(User)\
+            .filter_by(session_cookie=session_token)\
+            .filter(User.session_expiry_datetime >= datetime.datetime.now())\
+            .first()
+
+        request.user = user
+        return func(*args, **kwargs)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
+
 def getPath():
     return request.path or "/"
 
 @app.route('/', methods=["GET"])
 def index():
-    user = getUserByCookie()
-    return render_template("index.html", redirectTo=getPath(), user=user)
+    return render_template("index.html", redirectTo=getPath())
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        # right way to find user with correct password
+        user = db.query(User)\
+             .filter(User.username == username, User.password_hash == password_hash)\
+             .first()
+
+
+        session_cookie = str(uuid.uuid4())
+        expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=COOKIE_DURATION)
+
+        if user is None:
+            app.logger.info(f"User {username} failed to login with wrong password.")
+            redirect_url = request.args.get('redirectTo')
+            return redirect(url_for('login', redirectTo=redirect_url, error="Wrong User or Password"))
+
+        else:
+            user.session_cookie = session_cookie
+            user.session_expiry_datetime = expiry_time
+            db.add(user)
+            db.commit()
+            app.logger.info(f"User {username} is logged in")
+
+        redirect_url = request.args.get('redirectTo')
+        response = make_response(redirect(redirect_url))
+        response.set_cookie(WEBSITE_LOGIN_COOKIE_NAME, session_cookie, httponly=True, samesite='Strict')
+        return response
+
+    elif request.method == "GET":
+        cookie = request.cookies.get(WEBSITE_LOGIN_COOKIE_NAME)
+        user = None
+
+        if cookie is not None:
+            user = db.query(User) \
+                .filter_by(session_cookie=cookie) \
+                .filter(User.session_expiry_datetime >= datetime.datetime.now())\
+                .first()
+
+        if user is None:
+            logged_in = False
+        else:
+            logged_in = True
+
+        return render_template("login.html", logged_in=logged_in)
+
+@app.route('/registration', methods=["GET", "POST"])
+@provide_user
+
+def registration():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -68,91 +136,72 @@ def login():
         if password != repeat:
             return "Password and Repeat do not match! Please try again."
 
-        # query, check if there is a user with this username in the DB
-        # user = db.query(User).filter(User.username == username).one()  # -> needs to find one, otherwise raises Error
-        # user = db.query(User).filter(User.username == username).first()  # -> find first entry, if no entry, return None
-        # users = db.query(User).filter(User.username == username).all()  # -> find all, always returns list. if not entry found, empty list
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
 
-        user = db.query(User).filter(User.username == username).first()
         session_cookie = str(uuid.uuid4())
         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=COOKIE_DURATION)
-        if user is None:
-            password_hash = hashlib.sha256(password.encode()).hexdigest()
-            user = User(username=username,
-                        password_hash=password_hash,
-                        session_cookie=session_cookie,
-                        session_expiry_datetime=expiry_time)
-            db.add(user)
-            db.commit()
-            app.logger.info(f"User {username} is registered")
-        else:
-            user.session_cookie = session_cookie
-            user.session_expiry_datetime = expiry_time
-            db.add(user)
-            db.commit()
-            app.logger.info(f"User {username} is logged in")
 
-        redirect_url = request.args.get("redirectTo")
+        user = User(username=username,
+                    password_hash=password_hash,
+                    session_cookie=session_cookie,
+                    session_expiry_datetime=expiry_time)
+        db.add(user)
+        db.commit()
+        app.logger.info(f"User {username} is registered.")
+
+        redirect_url = "/"
         response = make_response(redirect(redirect_url))
         response.set_cookie(WEBSITE_LOGIN_COOKIE_NAME, session_cookie, httponly=True, samesite='Strict')
         return response
 
     elif request.method == "GET":
-        cookie = request.cookies.get(WEBSITE_LOGIN_COOKIE_NAME)
-        user = None
-        logged_in = False
-
-        if cookie is not None:
-            user = db.query(User) \
-                .filter_by(session_cookie=cookie) \
-                .filter(User.session_expiry_datetime >= datetime.datetime.now())\
-                .first()
-
-        if user is not None:
-            logged_in = True
-
-        return render_template("login.html", logged_in=logged_in)
-
+        return render_template("register.html")
 
 @app.route('/about', methods=["GET"])
-@require_session_token
+@provide_user
 def about():
-    user = getUserByCookie()
-    return render_template("about.html", user=user)
+    return render_template("about.html", redirectTo=getPath())
 
 
 @app.route('/faq', methods=["GET"])
 @require_session_token
 def faq():
-    user = getUserByCookie()
-    return render_template("faq.html", user=user)
+    return render_template("faq.html", redirectTo=getPath())
 
 
 @app.route('/logout', methods=["GET"])
-@require_session_token
+@provide_user
 def logout():
-    user = request.user
-    user.session_expiry_datetime = None
-    user.session_cookie = None
-    db.add(user)
-    db.commit()
-    app.logger.info(f"User {user.username} has logged out")
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie(WEBSITE_LOGIN_COOKIE_NAME, expires=0)
+
+    user = db.query(User)\
+        .filter_by(username=request.user.username)\
+        .first()
+
+    if user is not None:
+        # reset user
+        user.session_expiry_datetime = None
+        user.session_cookie = None
+        db.add(user)
+        db.commit()
+        app.logger.info(f"{user.username} has logged out.")
+
+    return response
 
 
 @app.route('/blog', methods=["GET", "POST"])
 @require_session_token
 def blog():
-    user = getUserByCookie()
+
     current_user = request.user
 
     if request.method == "POST":
-        title = request.form.get("title")
-        content = request.form.get("content")
+        title = request.form.get("posttitle")
+        text = request.form.get("posttext")
         post = Post(
-            title=title, content=content,
-            user=current_user,
-            datetime=datetime.datetime.now()
+            title=title, text=text,
+            user=current_user
         )
         db.add(post)
         db.commit()
@@ -160,23 +209,21 @@ def blog():
 
     if request.method == "GET":
         posts = db.query(Post).all()
-        return render_template("blog.html", posts=posts, user=user)
+        return render_template("blog.html", posts=posts, redirectTo=getPath())
 
 
 @app.route('/posts/<post_id>', methods=["GET", "POST"])
 @require_session_token
 def posts(post_id):
-    user = getUserByCookie()
     current_user = request.user
     post = db.query(Post).filter(Post.id == post_id).first()
 
     if request.method == "POST":
-        content = request.form.get("content")
+        text = request.form.get("text")
         comment = Comment(
-            content=content,
+            text=text,
             post=post,
-            user=current_user,
-            datetime=datetime.datetime.now()
+            user=current_user
         )
         db.add(comment)
         db.commit()
@@ -184,7 +231,8 @@ def posts(post_id):
 
     elif request.method == "GET":
         comments = db.query(Comment).filter(Comment.post_id == post_id).all()
-        return render_template('posts.html', post=post, comments=comments, user=user)
+        return render_template('posts.html', post=post, comments=comments, redirectTo=getPath())
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=7890)
--- model.py
+++ model.py
@@ -1,36 +1,56 @@
+import datetime
+
 from sqla_wrapper import SQLAlchemy
 
 db = SQLAlchemy("sqlite:///blog.sqlite")
 
 
+# 3 Klassen:
+# - Post
+# - User
+# - Comment
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
     username = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
     session_cookie = db.Column(db.String(255), nullable=True, unique=True)
     session_expiry_datetime = db.Column(db.DateTime)
 
+    posts = db.relationship('Post', backref='users')
+    comments = db.relationship('Comment', backref='users')
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
     title = db.Column(db.String)
-    content = db.Column(db.String)
-    datetime = db.Column(db.DateTime)
+    text = db.Column(db.String)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user = db.relationship(User)
 
+    comments = db.relationship('Comment', backref='posts')
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.String)
-    datetime = db.Column(db.DateTime)
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+    text = db.Column(db.String)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user = db.relationship(User)
 
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
     post = db.relationship(Post)
+
 
 if __name__ == '__main__':
     db.create_all()
